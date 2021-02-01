11dbc2a81d5802fdab631642f9c42b7e70cd04b1
ChrisPfeuffer <chris_pfeuffer@web.de>
2021-02-01 14:23:19 +0100

    Making the enemy spawn at Top in a Random Position

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    float _speed = 4.0f;
+    float _bottomBorder = -5.5f;
+    float _topBorder = 5.5f;
+    float _maxSpawnRight = 8.5f;
+    float _maxSpawnLeft = -8.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y <= _bottomBorder)
+        {
+            this.transform.position = new Vector3(Random.Range(_maxSpawnLeft, _maxSpawnRight), _topBorder, 0);
+        }
+    }
+}
